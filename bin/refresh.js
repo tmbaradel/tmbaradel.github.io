c369b8518860cb9c29154ce39f912c6716ec374f
th.baradel <th.baradel@DEAWS5232.home>
2022-04-05 11:06:32 +0200

    bin

---
+++ bin/refresh.js
@@ -0,0 +1,29 @@
+const reader = require('g-sheets-api');
+const fs = require('fs');
+var content = 'const points = [';
+
+const readerOptions = {
+    sheetId: "1JXrJ0FyimyhG7OJvW3qOVwvosAqfM-powfDNElZ9RqY",
+    apiKey: 'AIzaSyBV2UuvISeE1_pH84CtBabFyz40rGofV5M',
+    returnAllResults: true,
+    filter: {
+      "key to filter on": "value to match",
+    },
+};
+
+reader(readerOptions, (results, error) => {
+    if (error) {
+        console.error('problem retreaving points', error);
+    }
+    results.forEach(element => {
+        content += '{ name: "' + element.name + '", ring: "'+ element.ring +'",quadrant: "' + element.quadrant +'" },\n';
+    });
+    content += ']';
+    fs.writeFile('../points.js', content, err => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        console.log("done");
+    });
+});
